bdad204c836bb651cdb2f13b8d4fe729c9ca451f
Emilio <emilio.silva1@hotmail.com>
2019-08-10 21:21:06 -0400

    Inicio publicacion

---
+++ src/proyectopuntosflotantes/ProyectoPuntosFlotantes.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopuntosflotantes;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ *
+ * @author Emilio
+ */
+public class ProyectoPuntosFlotantes extends Application {
+    
+    
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws TwitterException {
+        
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+    cb.setDebugEnabled(true)
+      .setOAuthConsumerKey("hdRHBpZk8bgGkERmuNc78vmli")
+      .setOAuthConsumerSecret("viguyMV46wSk2jroiZjttgtbihTizYcOwosOsPbfnv61gjiBYN")
+      .setOAuthAccessToken("2344321298-oSPBpnAYRp0YBZ6qW6hr1JKXmOO7butDGnCmNRd")
+      .setOAuthAccessTokenSecret("ZAsWCrbotgTBcWQzJC82ixRgFagnWFS9tUJFElNoD35eQ");
+    TwitterFactory tf = new TwitterFactory(cb.build());
+    Twitter twitter = tf.getInstance();
+        
+    twitter.updateStatus("me gustan las galletas");
+    
+        launch(args);
+    }
+    
+}
